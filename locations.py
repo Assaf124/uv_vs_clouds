8d22b339297fcc3a0012756f3bec1dae4e3f7534
ASSAF ALONI <39213477+Assaf124@users.noreply.github.com>
2021-05-31 21:24:07 +0300

    Create locations.py
    first commit

---
+++ locations.py
@@ -0,0 +1,26 @@
+locations = dict()
+locations['Tel-Aviv'] = {'id': 0, 'Country': 'Israel', 'Latitude': 32.0853, 'Longitude': 34.7818, 'uri': 'il/tel-aviv/215854/current-weather/215854'}
+locations['London'] = {id: 1, 'Country': 'UK', 'Latitude': 51.5074, 'Longitude': -0.1278, 'uri': 'gb/london/ec4a-2/current-weather/328328'}
+locations['Tirana'] = {'id': 2, 'Country': 'Albania', 'Latitude': 41.3275, 'Longitude': 19.8187, 'uri': 'al/tirana/6522/current-weather/6522'}
+locations['Amsterdam'] = {'id': 4, 'Country': 'Netherlands', 'Latitude': 52.3667, 'Longitude': 4.8945, 'uri': 'nl/amsterdam/249758/current-weather/249758'}
+locations['Lisbon'] = {'id': 5, 'Country': 'Portugal', 'Latitude': 38.7223, 'Longitude': -9.1393, 'uri': 'pt/lisbon/274087/current-weather/274087'}
+
+# locations['Madrid'] = {'id': 6, 'Country': 'Spain', 'Latitude': 40.4168, 'Longitude': -3.7038, 'uri': 'es/madrid/308526/current-weather/308526'}
+# locations['Valencia'] = {'id': 7, 'Country': 'Spain', 'Latitude': 39.4699, 'Longitude': -0.3763, 'uri': 'es/valencia/310683/current-weather/310683'}
+# locations['Barcelona'] = {'id': 8, 'Country': 'Spain', 'Latitude': 41.3851, 'Longitude': 2.1734, 'uri': 'es/barcelona/307297/current-weather/307297'}
+# locations['Tunis'] = {'id': 9, 'Country': 'Tunisia', 'Latitude': 36.8065, 'Longitude': 10.1815, 'uri': 'tn/tunis/321398/current-weather/321398'}
+# locations['Rome'] = {'id': 10, 'Country': 'Italy', 'Latitude': 41.9028, 'Longitude': 12.4964, 'uri': 'it/rome/213490/current-weather/213490'}
+# locations['Athens'] = {'id': 11, 'Country': 'Greece', 'Latitude': 37.9838, 'Longitude': 23.7275, 'uri': 'gr/athens/182536/current-weather/182536'}
+# locations['Sofia'] = {'id': 12, 'Country': 'Bulgaria', 'Latitude': 42.6977, 'Longitude': 23.3219, 'uri': 'bg/sofia/51097/current-weather/51097'}
+# locations['Marseille'] = {'id': 13, 'Country': 'France', 'Latitude': 43.2965, 'Longitude': 5.3698, 'uri': 'fr/marseille/170960/current-weather/170960'}
+# locations['Florence'] = {'id': 14, 'Country': 'Italy', 'Latitude': 43.7696, 'Longitude': 11.2558, 'uri': 'it/florence/216189/current-weather/216189'}
+# locations['Milan'] = {'id': 15, 'Country': 'Italy', 'Latitude': 45.4642, 'Longitude': 9.1900, 'uri': 'it/milan/214046/current-weather/214046'}
+# locations['Lyon'] = {'id': 16, 'Country': 'France', 'Latitude': 45.7640, 'Longitude': 4.8357, 'uri': 'fr/lyon/171210/current-weather/171210'}
+# locations['Zagreb'] = {'id': 17, 'Country': 'Croatia', 'Latitude': 45.8150, 'Longitude': 15.9819, 'uri': 'hr/zagreb/117910/current-weather/117910'}
+# locations['Stockholm'] = {'id': 18, 'Country': 'Sweden', 'Latitude': 59.3293, 'Longitude': 18.0686, 'uri': 'se/stockholm/314929/current-weather/314929'}
+
+# locations['New-York'] = {'id': 18, 'Country': 'USA', 'Latitude': 40.7128, 'Longitude': -74.0060, 'uri': 'us/new-york/10007/current-weather/349727'}
+# locations['San-Diego'] = {'id': 19, 'Country': 'USA', 'Latitude': 32.7157, 'Longitude': -117.1611, 'uri': 'us/san-diego/92101/current-weather/347628'}
+# locations['Phoenix'] = {'id': 20, 'Country': 'USA', 'Latitude': 33.4484, 'Longitude': -112.0740, 'uri': 'us/phoenix/85003/current-weather/346935'}
+# locations['Dallas'] = {'id': 21, 'Country': 'USA', 'Latitude': 32.7767, 'Longitude': -96.7970, 'uri': 'us/dallas/75202/weather-forecast/351194'}
+# locations['Atlanta'] = {'id': 22, 'Country': 'USA', 'Latitude': 33.7490, 'Longitude': -84.3880, 'uri': 'us/atlanta/30303/weather-forecast/348181'}
